98e273bfb0715bcbcf7dcad73082c9c24696f526
loogeyy <53581729+loogeyy@users.noreply.github.com>
2022-06-27 21:02:33 -0700

    edit profile fields update automatically, added branding

--- app/src/main/java/com/example/inharmony/fragments/EditProfileFragment.java
+++ app/src/main/java/com/example/inharmony/fragments/EditProfileFragment.java
@@ -1,14 +1,22 @@
 package com.example.inharmony.fragments;
 
+import static android.app.Activity.RESULT_OK;
+
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -114,11 +122,11 @@ public class EditProfileFragment extends Fragment {
                 etName.setText(ParseUser.getCurrentUser().get("name").toString());
 
                 ParseFile profilePic = (ParseFile) ParseUser.getCurrentUser().get("profilePic");
-            if (profilePic != null) {
-                Glide.with(getContext()).load(profilePic.getUrl()).into(ivChangeProfilePic);
-            } else {
-                ivChangeProfilePic.setImageResource(R.drawable.nopfp);
-            }
+                if (profilePic != null) {
+                    Glide.with(getContext()).load(profilePic.getUrl()).into(ivChangeProfilePic);
+                } else {
+                    //ivChangeProfilePic.setImageResource(R.drawable.nopfp);
+                }
 
         }
 
@@ -154,15 +162,25 @@ public class EditProfileFragment extends Fragment {
             }
         });
 
-        checkButtonClicked();
+        checkUpdatePhotoButtonClicked();
+        checkUpdateProfileButtonClicked();
     }
 
     public static Intent createIntent(Context context) {
         return new Intent(context, EditProfileFragment.class);
     }
 
+    private void checkUpdatePhotoButtonClicked() {
+        btnChangePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchCamera();
+            }
+        });
 
-    private void checkButtonClicked() {
+    }
+
+    private void checkUpdateProfileButtonClicked() {
         btnUpdateProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -210,6 +228,7 @@ public class EditProfileFragment extends Fragment {
                 }
                 // only update user in the database
                 else {
+                    Log.i("NOT NEw SIGN UP", "WORK PLEASE");
                     if (!TextUtils.isEmpty(etName.getText())) {
                         name = etName.getText().toString();
                         ParseUser.getCurrentUser().put("name", name);
@@ -228,12 +247,19 @@ public class EditProfileFragment extends Fragment {
                         ParseUser.getCurrentUser().put("age", age);
                     }
 
-                    if (selectedGenres.length() != 0) {
-                        ((TextView)genres.getSelectedView()).setError("Please select up to 3 genres.");
+                    selectedGenres = getSelectedGenres();
+                    Log.i("SELECTED GENRES", String.valueOf(selectedGenres));
+                    if (selectedGenres.length() == 0) {
+                        ((TextView) genres.getSelectedView()).setError("Please select up to 3 genres.");
                         ((TextView) genres.getSelectedView()).setText("Please select up to 3 genres.");
-                        selectedGenres = getSelectedGenres();
-                        ParseUser.getCurrentUser().put("favGenre", selectedGenres);
+                        return;
                     }
+                    ParseUser.getCurrentUser().put("favGenres", selectedGenres);
+
+                    if (photoFile != null) {
+                        ParseUser.getCurrentUser().put("profilePic", new ParseFile(photoFile));
+                    }
+                    ParseUser.getCurrentUser().saveInBackground();
                 }
 
                 Intent i = new Intent(getContext(), MainActivity.class);
@@ -308,6 +334,9 @@ public class EditProfileFragment extends Fragment {
         user.put("name", name);
         user.put("age", age);
         user.put("favGenres", selectedGenres);
+        if (photoFile != null) {
+            user.put("profilePic", new ParseFile(photoFile));
+        }
 
         user.signUpInBackground(new SignUpCallback() {
             @Override
@@ -331,4 +360,60 @@ public class EditProfileFragment extends Fragment {
         }
         return list;
     }
+
+    private void launchCamera() {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            // picture taken
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                ivChangeProfilePic.setImageBitmap(takenImage);
+            } else { // Result was a failure
+                Toast.makeText(getContext(), "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d("getPhotoUri", "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        return new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+    }
+
+}
